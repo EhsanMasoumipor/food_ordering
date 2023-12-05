565144cd6be76c64fc3e6ac936d875ea8ef6b983
EhsanMasoumipor <emasoumipor@gmail.com>
2023-12-05 21:55:11 +0330

    update profile API finished

--- src/models/User.js
+++ src/models/User.js
@@ -9,5 +9,10 @@ const userSchema = new Schema({
     password: {
         type: String,
     },
+    phone: { type: String },
+    streetAddress: { type: String },
+    postalCode: { type: String },
+    city: { type: String },
+    country: { type: String },
 }, { timestamps: true });
 export const User = models?.User || model("User", userSchema);
