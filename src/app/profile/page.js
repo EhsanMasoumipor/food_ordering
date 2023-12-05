771199636f62b9510a62672d83a7d462776778f9
amir <amir11.vosoughi@gmail.com>
2023-12-05 18:08:15 +0330

    profile ui

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,23 +2,35 @@
 import Image from 'next/image';
 import { useSession } from "next-auth/react"
 import { redirect } from "next/navigation";
+import { useState } from 'react';
 
 export default function ProfilePage() {
-    const section = useSession()
-    const {status} = section
+    const session = useSession()
+    const  [userName , setUserName] = useState(session?.data?.user?.name)
+    const {status} = session
 
     if(status === 'loading') {
         return 'Loading...';
+    }
+    async function handleProfileInfoUpdate (ev) {
+        ev.preventDefault();
+        const response = await fetch('/api/profile'  ,{
+            method : "PUT",
+            headers : {
+                'Content-type' : "application/json"
+            },
+            body : JSON.stringify({ name : userName})
+        })
     }
 
     if(status === 'unauthenticated'){
         return redirect('/login')
     }
-    const userImage = section.data.user.image
+    const userImage = session.data.user.image
     return(
         <section className="mt-8">
             <h1 className="text-center text-primary text-4xl font-semibold my-4">Profile</h1>
-            <form className="max-w-md mx-auto ">
+            <div className="max-w-md mx-auto ">
                 <div className='flex gap-2 items-center'>
                     <div>
                         <div>
@@ -26,12 +38,13 @@ export default function ProfilePage() {
                             <button>Edit</button>
                        </div>
                     </div>
-                    <div className='grow'>
-                        <input type='text' placeholder='First and last name' />
+                    <form className='grow' onSubmit={handleProfileInfoUpdate}>
+                        <input type='text' placeholder='First and last name' value={userName} onChange={ev => setUserName(ev.target.value)}  />
+                        <input type='email' disabled value={session.data.user.email} />
                         <button type='submit'>save</button>
-                    </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </section>
     )
 }
