cd1dc2c8628c9ff3f325fb851dd12d8f7abb50a9
EhsanMasoumipor <emasoumipor@gmail.com>
2023-12-06 13:07:30 +0330

    Admin - users APIs are reasy

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -7,23 +7,38 @@ import { UserInfo } from "@/models/UserInfo";
 export async function PUT(request) {
     mongoose.connect(process.env.MONGO_URI);
     const data = await request.json();
-    const { name, ...otherUserInfo } = data;
-    const session = await getServerSession(authOptions);
-    const email = session.user.email
-    await User.findOneAndUpdate({ email }, { name });
-    await UserInfo.findOneAndUpdate({ email }, otherUserInfo);
-
+    const { _id, name, ...otherUserInfo } = data;
+    let filter = {};
+    if (_id) {
+        filter = { _id };
+    } else {
+        const session = await getServerSession(authOptions);
+        const email = session.user.email
+        filter = { email };
+    }
+    const user = await User.findOne(filter);
+    await User.findOneAndUpdate(filter, { name }, { upsert: true });
+    await UserInfo.findOneAndUpdate({ email: user.email }, otherUserInfo);
     return Response.json(true);
 }
 
-export async function GET() {
+export async function GET(request) {
     mongoose.connect(process.env.MONGO_URI);
-    const session = await getServerSession(authOptions);
-    const email = session?.user?.email
-    if (!email) {
-        return Response.json({})
+    const url = new URL(request.url);
+    const _id = url.searchParams.get('_id');
+    let filterUser = {};
+    if (_id) {
+        filterUser = _id;
+    } else {
+        const session = await getServerSession(authOptions);
+        const email = session?.user?.email
+        if (!email) {
+            return Response.json({})
+        }
+        filterUser = email
     }
-    const user = await User.findOne({ email })
-    const userInfo = await UserInfo.findOne({ email });
+    const user = await User.findOne(filterUser).lean()
+    const userInfo = await UserInfo.findOne({ email: user.email }).lean();
     return Response.json({ ...user, ...userInfo });
+
 }
